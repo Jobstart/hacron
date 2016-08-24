f792d0a97c25cd9dbc2a04e403e3e1dc027141ed
Andrew E. Rhyne <rhyneandrew@gmail.com>
2016-08-24 13:52:28 -0700

    fix

--- src/ssl.js
+++ src/ssl.js
@@ -18,19 +18,25 @@ const ETCD_SSL_PATH: string = '/opt/ssl/etcd.pem';
 if (!amqp) {
   try {
     amqp = fs.readFileSync(AMQP_SSL_PATH);
-    logger.info('using ssl for amqp');
-  } catch (e) {
-    logger.info('not using ssl for amqp');
-  }
+  } catch (e) {}
 }
 
 if (!etcd) {
   try {
     etcd = fs.readFileSync(ETCD_SSL_PATH);
-    logger.info('using ssl for etcd');
-  } catch (e) {
-    logger.info('not using ssl for etcd');
-  }
+  } catch (e) {}
+}
+
+if (amqp) {
+  logger.info('using ssl for amqp');
+} else {
+  logger.info('not using ssl for amqp');
+}
+
+if (etcd) {
+  logger.info('using ssl for etcd');
+} else {
+  logger.info('not using ssl for etcd');
 }
 
 export {
